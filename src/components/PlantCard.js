62d75a86062feb946bd37ca5b1ff26ee08c072ad
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-08-11 10:44:27 -0500

    implement button event for out of stock items

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function PlantCard({
   plant: { id, name, image, price},
 }) {
+
+  const [inStock, setInStock] = useState(true)
+
+  function onChangeStock(){
+    setInStock(prev => !prev)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button onClick={onChangeStock} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
