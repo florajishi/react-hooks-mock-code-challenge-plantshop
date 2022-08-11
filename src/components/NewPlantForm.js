78ed225d3714da567d45f73c33772bb1879f02bf
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-08-11 11:06:11 -0500

    add form for new plants

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 
 function NewPlantForm() {
-  const [plantForm, setPlantFormData] = useState({})
+  const [plantForm, setPlantFormData] = useState({
+    name: "",
+    image: "",
+    price: ""
+  })
 
-  function onSubmit(){
-    
+  function handleChange(e){
+    setPlantFormData(prevFormData => {
+      return({
+        ...prevFormData,
+        [e.target.name]:e.target.value
+      })
+    })
   }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(plantForm)
+    })
+    .then(r => r.json())
+    .then(newPlant => console.log(newPlant))
+  }
+
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
-        <button type="submit">Add Plant</button>
+        <input onChange={handleChange} type="text" name="name" placeholder="Plant name" />
+        <input onChange={handleChange} type="text" name="image" placeholder="Image URL" />
+        <input onChange={handleChange} type="number" name="price" step="0.01" placeholder="Price" />
+        <button onSubmit={handleSubmit} type="submit">Add Plant</button>
       </form>
     </div>
   );
